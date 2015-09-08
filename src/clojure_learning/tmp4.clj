ad3d3fe6fa8be3919f2f83044b44d95ecf997360
cclaudiulbs <claudiu.cosar.lbs@gmail.com>
2015-09-09 00:29:00 +0300

    4clojure:: 2nd attempt to impl the gen-palindromic lazy seq

---
+++ src/clojure_learning/tmp4.clj
@@ -0,0 +1,58 @@
+(defn palindrome? [x]
+  (loop [init x
+         reversed 0]
+    (if (not= 0 init)
+      (recur (quot init 10) (+ (* reversed 10) (mod init 10)))
+      (= x reversed))))
+
+(palindrome? 2) ;; true
+(palindrome? 101) ;; true
+(palindrome? 13) ;; false
+
+(defn gen-palindromes
+  [start]
+  (letfn [(palindrome? [x]
+            (loop [init x
+                   reversed 0]
+              (if (not= 0 init)
+                (recur (quot init 10) (+ (* reversed 10) (rem init 10)))
+                (= x reversed))))
+          (gen-palins [x]
+            (if (palindrome? x)
+              (cons x (lazy-seq (gen-palins (inc x))))
+              (recur (inc x))))]
+    (gen-palins start)))
+
+(gen-palindromes 1234550000)
+(take 26 (gen-palindromes 0))
+
+(doc frequencies)
+(frequencies [1 2 3 1 5 1 5])
+
+(import java.util.Date)
+(do
+  (println (Date. (System/currentTimeMillis)))
+  (take 20 (gen-palindromes 1234550000))
+  (println (Date. (System/currentTimeMillis))))
+
+(class (gen-palindromes 1234550000)) ;; -> not lazy
+(= (first (gen-palindromes (* 111111111 111111111)))
+   (* 111111111 111111111))
+
+(defn gen-palindromes
+  [start]
+  (letfn [(palindrome? [x]
+            (loop [init x
+                   reversed 0]
+              (if (not= 0 init)
+                (recur (quot init 10) (+ (* reversed 10) (mod init 10)))
+                (= x reversed))))
+          (gen-seq [x]
+            (lazy-seq
+               (cons x (gen-seq (inc x)))))]
+    (filter palindrome? (iterate inc start))))
+
+(filter palindrome? (range))
+(gen-palindromes 1234550000)
+;; 00:08:58
+;; 00:10:58
