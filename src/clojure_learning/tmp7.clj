74eaa62a67f307d344c3bea10ae467e3c156277c
cclaudiulbs <claudiu.cosar.lbs@gmail.com>
2015-10-06 21:33:59 +0300

    some monadic + FP patterns playing around

--- src/clojure_learning/tmp7.clj
+++ src/clojure_learning/tmp7.clj
@@ -52,3 +52,60 @@
 (take-until 10 [1 2 [3 [4 5] 6] 7]) ;; [1 2 3 4]
 (take-until 9 (range))              ;; [0 1 2 3]
 (take-until 30 [1 2 [3 [4 [5 [6 [7 12 13]] 9]] 10] 11]) ;; [1 2 [3 [4 [5 [5 [7]]]]]]
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; the wrapper functional design-pattern
+;;;;;;;;;;;;;;;;;;;;;;;;;;
+(defn wrapper [func]
+  (fn [& args] (apply func args)))
+
+(defn add [a b] (+ a b))
+(defn minus [a b] (- a b))
+
+(defn add-wrapper [a b]
+  ((-> add wrapper) a b))
+
+(add-wrapper 2 2) ;; 4
+(add-wrapper 2 3) ;; 5
+
+(defn minus-wrapper [& args]
+  (apply (-> minus wrapper) args))
+
+(minus-wrapper 3 1) ;; 2
+
+;; always remember that curry is the process of transforming a multi-argument function, into a chain of
+;; single argument function, each function taking exactly ONE of the arguments, and returning another
+;; function which takes the next argumeent until the argument-list is exhausted.
+;; while partial application is the process of binding SOME of the arguments to some values, while returning
+;; another function which behaves like the one initially applied, but the returned function takes the REST of
+;; the arguments.
+
+;; building on the previous wrappers, the sequence monad would look like:
+;; every monad had two HOFs: unit + bind
+(defn unit [op-result]
+  (str "this is a higher order Type which is not compatible with the op arg-types: " op-result))
+(defn bind [func]
+  (fn [args]
+    (if (every? integer? args)
+      (apply func args)
+      (throw (IllegalArgumentException. (str "Not a number!" args))))))
+
+;; define base-operations
+(defn add [a b] (+ a b))
+(defn minus [a b] (- a b))
+
+(defn lift-add [add & args]
+  ((bind add) args))
+(defn lift-minus [minus & args]
+  ((bind minus) args))
+
+(def five (lift-add add 2 3))
+(println five)
+
+(def two (lift-minus minus 5 3))
+(println two)
+
+(def wrong-input (lift-add "str" 2)) ;; -> compiler exception
+
+(-> (lift-minus minus 3 1)
+    unit)
